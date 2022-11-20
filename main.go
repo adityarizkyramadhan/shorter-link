bb6233e7bb84cb9466ad63401995e64c447e75c1
adityarizkyramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2022-11-20 23:30:20 +0700

    refactor cors

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -40,7 +39,7 @@ func main() {
 
 	router := gin.Default()
 
-	router.Use(cors.Default())
+	router.Use(cORS())
 
 	router.POST("", func(ctx *gin.Context) {
 		c := ctx.Request.Context()
@@ -83,6 +82,20 @@ func main() {
 	})
 	router.Run(":10001")
 
+}
+
+var cORS = func() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
 }
 
 type driverSupabase struct {
